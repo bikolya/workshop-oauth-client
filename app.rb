c6f4d45ba1f543c6d6eecc4fe11785f7f81c5629
Nikolay Bienko <bikolya@gmail.com>
2016-11-23 21:12:38 +0300

    Replace Foursquare OAuth with own OAuth server

--- app.rb
+++ app.rb
@@ -3,45 +3,33 @@ require 'httparty'
 require 'json'
 
 class OAuthFoursquare < Sinatra::Base
-  CLIENT_ID = "XM0GA5B502NZFRWLT1FRRUH0IFKBTSTD3SVYJLFMTFKS3VSN"
-  CLIENT_SECRET = "GFKBO5GYASON4NK42WBYUVBVTCNFNLYH2LK5JZ1F1N50MMU4"
+  CLIENT_ID = "52660e807dcc2fb72d31df8c780f8805fb92fbecb00d2789cfa7282ed29160bc"
+  CLIENT_SECRET = "03e82b73fd7f2fc6c11e95680193de40ebcda178b53a1b793eb324bbeb5e5321"
   REDIRECT_URI = "http://localhost:9292/callback"
-  AUTH_URL = "https://foursquare.com/oauth2/authenticate?client_id=#{CLIENT_ID}&response_type=code&redirect_uri=#{REDIRECT_URI}"
+  AUTH_URL = "http://localhost:3000/oauth/authorize?client_id=#{CLIENT_ID}&response_type=code&redirect_uri=#{REDIRECT_URI}"
 
   enable :sessions
 
   get '/login' do
-    "<a href=#{AUTH_URL}> Auth via Foursquare </a>"
-  end
-
-  get '/' do
-    resp = HTTParty.get(
-      "https://api.foursquare.com/v2/users/self/tips",
-      query: {
-        oauth_token: session[:token],
-        v: "20161122"
-      }
-    )
-    data = JSON.parse(resp.body)
-    erb :index, locals: { tips: data['response']['tips']['items'] }
+    "<a href=#{AUTH_URL}> Auth via My Foursquare </a>"
   end
 
   get '/explore' do
+    redirect '/login' unless session[:token]
+
     resp = HTTParty.get(
-      "https://api.foursquare.com/v2/venues/explore",
-      query: {
-        near: 'Moscow, Russia',
-        oauth_token: session[:token],
-        v: "20161122"
+      "http://localhost:3000/api/venues",
+      headers: {
+        Authorization: "Bearer #{session[:token]}"
       }
     )
     data = JSON.parse(resp.body)
-    erb :explore, locals: { venues: data['response']['groups'].first['items'] }
+    erb :explore, locals: { venues: data }
   end
 
   get '/callback' do
-    resp = HTTParty.get(
-      "https://foursquare.com/oauth2/access_token",
+    resp = HTTParty.post(
+      "http://localhost:3000/oauth/token",
       query: {
         client_id: CLIENT_ID,
         client_secret: CLIENT_SECRET,
@@ -53,5 +41,17 @@ class OAuthFoursquare < Sinatra::Base
     hash = JSON.parse(resp.body)
     session[:token] = hash['access_token']
     redirect '/'
+  end
+
+  get '/' do
+    redirect '/login' unless session[:token]
+
+    resp = HTTParty.get(
+      "http://localhost:3000/api/me/",
+      headers: {
+        Authorization: "Bearer #{session[:token]}"
+      }
+    )
+    resp.body
   end
 end
